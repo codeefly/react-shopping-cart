357d657743e2eca873bb5bd258b6823989ed39a8
quixlab <quixlab.com@gmail.com>
2021-08-10 20:56:00 +0600

    calculation done

--- src/App.js
+++ src/App.js
@@ -22,13 +22,31 @@ function App() {
             setCartItems([...cartItems, { ...product, qty: 1 }]);
         }
     };
+
+    const onRemove = (product) => {
+        const exist = cartItems.find((x) => x.id === product.id);
+        if (exist.qty === 1) {
+            setCartItems(cartItems.filter((x) => x.id !== product.id));
+        } else {
+            setCartItems(
+                cartItems.map((x) =>
+                    x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+                )
+            );
+        }
+    };
+
     return (
         <div className="App">
             <Header />
 
             <div className="row">
                 <Main onAdd={onAdd} products={products} />
-                <Basket onAdd={onAdd} cartItems={cartItems} />
+                <Basket
+                    onRemove={onRemove}
+                    onAdd={onAdd}
+                    cartItems={cartItems}
+                />
             </div>
         </div>
     );
--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 const Basket = ({ cartItems, onAdd, onRemove }) => {
+    const itemPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
+    const taxPrice = itemPrice * 0.14;
+    const shippingPrice = itemPrice > 2000 ? 0 : 50;
+    const totalPrice = itemPrice + taxPrice + shippingPrice;
+
     return (
         <>
             <div className="block col-1">
@@ -26,6 +31,38 @@ const Basket = ({ cartItems, onAdd, onRemove }) => {
                         </div>
                     </div>
                 ))}
+
+                {cartItems.length !== 0 && (
+                    <>
+                        <hr />
+                        <div className="row">
+                            <div className="col-2">Item Price</div>
+                            <div className="col-1 text-right">
+                                ${itemPrice.toFixed(2)}
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2">Tax Price</div>
+                            <div className="col-1 text-right">
+                                ${taxPrice.toFixed(2)}
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2">Shipping Price</div>
+                            <div className="col-1 text-right">
+                                ${shippingPrice.toFixed(2)}
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2">
+                                <strong>Total Price</strong>
+                            </div>
+                            <div className="col-1 text-right">
+                                ${totalPrice.toFixed(2)}
+                            </div>
+                        </div>
+                    </>
+                )}
             </div>
         </>
     );
